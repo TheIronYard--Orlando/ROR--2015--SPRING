9055a20d3083a12d9383531f8198c9650efbc35a
Brian Gates <core.safecrlf>
2015-01-07 15:24:10 -0500

    Define what method should do

---
+++ Notes/Solved Problems/english_to_number.rb
@@ -0,0 +1,6 @@
+# I want a function that will convert English language numbers into 
+# their numerical representation.
+# For example:
+# english_to_number('one') #=> 1
+# english_to_number('ninety six') #=> 96
+# The function should at least work up to 100. 1_000_000_000_000 would be better.
