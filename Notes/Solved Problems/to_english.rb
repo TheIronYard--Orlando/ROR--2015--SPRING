f228d2a2f6a141c09851b2d813b31c1ae7d4f69e
Brian Gates <core.safecrlf>
2015-01-08 10:58:53 -0500

    get conversion to work with hash; about to try array refactor

--- Notes/Solved Problems/to_english.rb
+++ Notes/Solved Problems/to_english.rb
@@ -1,2 +1,17 @@
 # this is going to be a method that converts
 # integers into their English language representations.
+class Fixnum
+
+  def to_english
+    names = {
+      0 => "zero",
+      1 => "one",
+      2 => "two"
+    }
+    names[self]
+  end
+end
+
+puts 0.to_english #=> "zero"
+puts 1.to_english #=> "one"
+puts 2.to_english #=> "two"
