b587e8aee741f5c5b23e185aea3bb259fa7cec44
Brian Gates <core.safecrlf>
2015-01-08 15:44:40 -0500

    game exists and has grid

--- Notes/game.rb
+++ Notes/game.rb
@@ -2,5 +2,12 @@ class Game
   # this is going to be a tic-tac-toe game.
   # it's going to be awesome.
   # we'll play this within irb, by making moves
-  # until somebody wins.    
+  # until somebody wins.
+  attr_accessor :grid #this means we can
+  #interact with the instance variable @grid
+  #and we can pretend it's a method: Game.new.grid
+
+  def initialize #this runs when Game.new is called
+    @grid = :a_thing_which_exists
+  end    
 end
--- Notes/game_test.rb
+++ Notes/game_test.rb
@@ -1,8 +1,14 @@
 require 'minitest/autorun'
-require 'minitest/pride'
+#require 'minitest/pride'
 require './game'
 
 class TestGame < MiniTest::Unit::TestCase
 
+  def test_game_exists
+    assert Game.new
+  end
 
+  def test_game_grid_exists
+    assert Game.new.grid
+  end
 end
