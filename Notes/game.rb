4c53afe100ef4459c0f1a6e30817f0fbec735c9c
Brian Gates <core.safecrlf>
2015-01-08 10:23:35 -0500

    initial commit

---
+++ Notes/game.rb
@@ -0,0 +1,6 @@
+class Game
+  # this is going to be a tic-tac-toe game.
+  # it's going to be awesome.
+  # we'll play this within irb, by making moves
+  # until somebody wins.    
+end
---
+++ Notes/game_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './game'
+
+class TestGame < MiniTest::Unit::TestCase
+
+
+end
