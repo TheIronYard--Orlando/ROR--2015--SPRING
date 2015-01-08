b7158f01e695e62cdabe8d66f4271a4147b1ed1b
Brian Gates <core.safecrlf>
2015-01-08 11:55:05 -0500

    works for *TWO* different three digit numbers

--- Notes/Solved Problems/to_english.rb
+++ Notes/Solved Problems/to_english.rb
@@ -7,12 +7,25 @@ class Fixnum
                         five six seven eight nine
                         ten eleven twelve thirteen
                         fourteen fifteen sixteen 
-                        seventeen eighteen nineteen twenty
+                        seventeen eighteen nineteen
                        )
-    if self > 20
+    if self > 99
+      remainder = self % 100
+      if remainder > 0
+        "one hundred #{remainder.to_english}"
+      else
+        "one hundred"
+      end
+    elsif self > 19
+      tens_names = %w(blank blank twenty thirty forty fifty
+                      sixty seventy eighty ninety)
       tens = self / 10
       ones = self % 10
-      "twenty #{ones.to_english}" 
+      if ones > 0
+        "#{tens_names[tens]} #{ones.to_english}" 
+      else
+        tens_names[tens]
+      end
     else
       names_as_array[self]  
     end 
@@ -23,5 +36,10 @@ puts 0.to_english #=> "zero"
 puts 1.to_english #=> "one"
 puts 2.to_english #=> "two"
 puts 9.to_english #=> "nine"
+puts 20.to_english #=> "twenty"
 puts 21.to_english #=> "twenty one"
 puts 22.to_english #=> "twenty two"
+puts 30.to_english #=> "thirty"
+puts 99.to_english #=> "ninety nine"
+puts 100.to_english #=> "one hundred"
+puts 101.to_english
