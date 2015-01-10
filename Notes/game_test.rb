7c81df68cb21902f4225cb6bde62fd246b8712be
Brian Gates <core.safecrlf>
2015-01-10 14:17:44 -0500

    now all tests pass

--- Notes/game_test.rb
+++ Notes/game_test.rb
@@ -29,29 +29,25 @@ class TestGame < MiniTest::Unit::TestCase
   end
 
   def test_x_goes_first
-    # now that I have a @current_player variable to allow taking turns,
-    # I can write this test.
     assert_equal 'X', @game.current_player
   end
 
-=begin These two tests are broken right now, so I'm commenting them out
   def test_making_first_move_puts_x_on_board
     @game.move!(0, 0)
-    assert @game.board.spaces.any?{|space| space == 'X'}
+    assert @game.board.spaces.flatten.any?{|space| space == 'X'}
   end
 
   def test_making_second_move_puts_o_on_board
     @game.move!(0, 0)
     puts "made 1st move"
     @game.move!(0, 1)
-    assert @game.board.spaces.any?{|space| space == 'O'}
+    assert @game.board.spaces.flatten.any?{|space| space == 'O'}
   end
-=end
+
   def test_second_move_must_be_different_space_from_first_move
-    # to get this to pass, I needed to tell move where to go.
     @game.move!(0, 0)
     @game.move!(0, 0)
-    assert @game.board.spaces.none?{|space| space == 'O'}
+    assert @game.board.spaces.flatten.none?{|space| space == 'O'}
   end
   
 end
